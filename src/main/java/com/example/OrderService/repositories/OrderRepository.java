e99430af199936dabd5566813ebf2655623a025e
lodjers <officialkovtun@gmail.com>
2025-03-15 13:30:20 +0300

    Удалены все файлы в новой ветке

--- src/main/java/com/example/OrderService/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.OrderService.repositories;
-
-import com.example.OrderService.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
